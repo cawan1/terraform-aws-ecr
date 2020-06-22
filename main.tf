ffb0db393868341007a8a2c2f274c93f741994f7
Cawan <cawan.porto@gmail.com>
2020-06-22 16:35:23 -0300

    changing repositories for images

--- main.tf
+++ main.tf
@@ -1,9 +1,8 @@
 resource "aws_ecr_repository" "repository" {
-  count = length(var.repositories)
-  name                 = "${var.project}/${var.repositories[count.index]}"
+  count = length(var.images)
+  name                 = "${var.project}/${var.images[count.index]}"
   image_tag_mutability = "MUTABLE"
   image_scanning_configuration {
     scan_on_push = false
   }
-
 }
--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "project" {
   description = "Project Name"
   #default = "facial"
 }
-variable "repositories" {
+variable "images" {
   type = list(string)
   #default = ["blink-recognition","blink-recognition-client","face-recognition"]
 }
