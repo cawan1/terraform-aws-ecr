dff8d62f3947c436edfe8e7644590091edd24ad3
Cawan <cawan.porto@gmail.com>
2020-06-22 23:20:11 -0300

    vars

--- variables.tf
+++ variables.tf
@@ -1,16 +1,7 @@
-variable "region" {
-    type = string
-    default = "us-east-1"
-    description = "AWS Region"
-}
 variable "project" {
     type = string
     description = "Project Name"
 }
-variable "environment" {
-    type = string
-}
-
 variable "images" {
     type = list(string)
     description = "List of images for repository"
