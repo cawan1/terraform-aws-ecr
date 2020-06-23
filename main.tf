0081408a7725662f2e0af1e23c2123640ca48dc0
Cawan <cawan.porto@gmail.com>
2020-06-22 23:06:20 -0300

    General+

--- main.tf
+++ main.tf
@@ -1,18 +1,3 @@
-provider "aws" {
-  version = "~> 2.0"
-  region  = "us-east-1"
-}
-
-terraform {
-  backend "remote" {
-    hostname     = "app.terraform.io"
-    organization = "accurate" 
-    workspaces {
-      name = "${var.project}-${var.environment}"
-    }
-  }
-}
-
 module "ecr" {
     source = "./module/ecr"
     project = var.project
