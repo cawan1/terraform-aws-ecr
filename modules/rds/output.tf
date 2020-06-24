fc0f54d3532dca59d6658fbf015920866790e95d
Cawan <cawan.porto@gmail.com>
2020-06-24 13:31:09 -0300

    Fix rds output

--- modules/rds/output.tf
+++ modules/rds/output.tf
@@ -1,19 +1,15 @@
-output "vpc_id" {
-  description = "The ID of the VPC"
-  value       = module.vpc.vpc_id
+
+output "rds_instance_endpoint" {
+  description = "Connection Endpoint"
+  value       = "${module.db.this_db_instance_endpoint}"
 }
 
-output "private_subnets" {
-  description = "List of IDs of private subnets"
-  value       = module.vpc.private_subnets
+output "rds_instance_name" {
+  description = "The database name"
+  value       = "${module.db.this_db_instance_name}"
 }
 
-output "public_subnets" {
-  description = "List of IDs of public subnets"
-  value       = module.vpc.public_subnets
-}
-
-output "database_subnets" {
-  description = "List of IDs of database subnets"
-  value       = module.vpc.database_subnets
+output "rds_instance_username" {
+  description = "The master username for the database"
+  value       = "${module.db.this_db_instance_username}"
 }
