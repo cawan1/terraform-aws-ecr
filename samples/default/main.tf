9bc168ebb31f1840faf12a8616e532c5b36a5a44
Cawan <cawan.porto@gmail.com>
2020-07-03 14:45:48 -0300

    cognito

--- samples/default/main.tf
+++ samples/default/main.tf
@@ -15,7 +15,7 @@ terraform {
 
 module  "accurate" {    
    source = "app.terraform.io/accurate/ecr/aws"    
-   version = "1.2.4"    
+   version = "1.2.5"    
    project = "myproject" #lower case required
    environment = "test"   
    }
